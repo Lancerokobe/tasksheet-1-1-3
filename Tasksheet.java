02a2c462d476fec81272b44f51f514214fa85abf
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-08-15 14:15:57 +0000

    H3110 w0r1d 2.0 true

--- Tasksheet.java
+++ Tasksheet.java
@@ -1,7 +1,13 @@
 class Tasksheet {
     public static void main(String[] args) {
         byte zero = 0;
-        String output = "W" + zero + "w";
+        short code = (short) 28 + 67 * (600/13);
+        float num = 2;
+        int no =  1;
+        char abc = 'r';
+        char aei = 'd';
+        boolean value = false;
+        String output = "H" + code + " w" + zero + abc + no + aei +" " + num +" "+ !value;
         System.out.println(output);
     }
 }
